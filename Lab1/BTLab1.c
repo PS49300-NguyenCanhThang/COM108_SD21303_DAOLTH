ae2a7aac1a3fe070e0eb1f69bf1f7258d2bbb9ac
PS49300_NGUYENCANHTHANG <canhthang457@gmail.com>
2025-11-06 21:18:50 +0700

    Update Lab1

---
+++ Lab1/BTLab1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    char ten[50];
+
+    printf("Nhap so a va b: ");
+    scanf("%d%d", &a, &b);
+
+    printf("Nhap vao ten toi: ");
+    scanf("%s", ten); // ✅ sửa lại từ %c thành %s
+
+    printf("a = %d, b = %d\n", a, b);
+    printf("Ten cua ban la: %s\n", ten);
+
+    return 0;
+}
